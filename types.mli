9aad1897a4bc1734b0552153b691f2dbcf843a27
rg545 <rg545@cornell.edu>
2018-03-29 18:08:57 -0400

    added comments to types.mli

--- types.mli
+++ types.mli
@@ -1,11 +1,16 @@
+(** This module handles all of the types that we need for this project*)
 type class'
 
+(** Represents a weapon type*)
 type wtype
 
+(** Represents the names of possible AI types*)
 type ai
 
+(** Represents each stat*)
 type stat = Health | Str | Mag | Def | Spd | Res | Skl | Lck
 
+(** Represents an item with stats and shop values*)
 type item = {
   name : string;
   wtype : wtype;
@@ -22,6 +27,8 @@ type item = {
   penalty : (stat * int * int) list;
 }
 
+
+(** Represents a character, its stats and other details*)
 type character = {
   name : string;
   class' : class';
@@ -39,6 +46,10 @@ type character = {
   mov : int;
   con : int;
   aid : int;
+  hit : int;
+  atk : int;
+  crit : int;
+  avoid : int;
   inv : item list;
   eqp : item;
   ability : string list;
@@ -46,6 +57,7 @@ type character = {
   wlevels : (wtype * char) list
 }
 
+(** Represents an enemy unit and its stats*)
 type enemy = {
   name : string;
   class' : class' ;
@@ -60,15 +72,22 @@ type enemy = {
   lck : int;
   mov : int;
   con : int;
+  hit : int;
+  atk : int;
+  crit : int;
+  avoid : int;
   ai : ai
 }
 
+(** Represents different types of terrain*)
 type terrain = Plain | Wall | Throne | Door | Chest | Defence | Forest |
                Village | Armory of item list | Shop of item list |
                Damaged_wall of int | Mountain | Ocean | Desert | Despawn
-
-type map = (int * int * terrain * character option) list
-
-type player_locations
-
-type enemy_locations
+(** Represents one tile on a map*)
+type tile = (int * int * terrain)
+(** Represents a map as a whole*)
+type map = (tile * character option) list
+(** Represents a list of all player unit locations*)
+type player_locations = (character * tile) list
+(** Representns a list of all enemy unit locations*)
+type enemy_locations = (enemy * tile) list

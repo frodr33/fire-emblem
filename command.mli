bc53f49c9edd92f50a832b7940999ab89ccbeafa
rg545 <rg545@cornell.edu>
2018-04-01 17:21:10 -0400

    actual push

--- command.mli
+++ command.mli
@@ -2,7 +2,6 @@ open State
 open Types
 
 
-type action
 (** [input i st] takes a button input and returns an appropriate action based on te
     current state [st]*)
 val input : 'input -> state -> action

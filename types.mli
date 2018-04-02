cc2f2e32f20835511d61dfe965b60ef8d6c2da4b
awt46 <awt46@cornell.edu>
2018-04-02 12:19:58 -0400

    Types

--- types.mli
+++ types.mli
@@ -7,13 +7,13 @@ type wtype = Sword
 (** Represents the names of possible AI types*)
 type ai
 
-(*Represents keyboard input *)
-type input
+type key = A|B|Start|Select|LT|RT|Up|Down|Left|Right|Nothing
+
+type action = Move|Attack|Idle|Invalid
+
 (** Represents each stat*)
 type stat = Health | Str | Mag | Def | Spd | Res | Skl | Lck
 
-
-type action
 (** Represents an item with stats and shop values*)
 type item = {
   name : string;
@@ -31,7 +31,13 @@ type item = {
   penalty : (stat * int * int) list;
 }
 
-
+(** Represents different types of terrain*)
+type terrain = Plain | Wall | Throne | Door | Chest | Defence | Forest |
+               Village | Armory of item list | Shop of item list |
+               Damaged_wall of int | Mountain | Ocean | Desert | Despawn
+(** Represents one tile on a map*)
+type tile = {location:int*int;terrain:terrain}
+(** Represents a map as a whole*)
 (** Represents a character, its stats and other details*)
 type character = {
   name : string;
@@ -58,7 +64,8 @@ type character = {
   eqp : item option;
   ability : string list;
   supports : (string * char) list;
-  wlevels : (wtype * char * int) list
+  wlevels : (wtype * char * int) list;
+  movement: tile list
 }
 
 (** Represents an enemy unit and its stats*)
@@ -83,13 +90,7 @@ type enemy = {
   ai : ai
 }
 
-(** Represents different types of terrain*)
-type terrain = Plain | Wall | Throne | Door | Chest | Defence | Forest |
-               Village | Armory of item list | Shop of item list |
-               Damaged_wall of int | Mountain | Ocean | Desert | Despawn
-(** Represents one tile on a map*)
-type tile = (int * int * terrain)
-(** Represents a map as a whole*)
+
 type map = (tile * character option) list
 (** Represents a list of all player unit locations*)
 type player_locations = (character * tile) list

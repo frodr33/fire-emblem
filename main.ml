d429ca096f8bd6f6ffc6473d1c21a309f48065b8
fsr32 <fsr32@cornell.edu>
2018-04-04 00:56:37 -0400

    updated main

---
+++ main.ml
@@ -0,0 +1,24 @@
+open Types
+
+module Html = Dom_html
+let js = Js.string (* partial function, takes in string *)
+let document = Html.document
+
+(************************ DOM HELPERS ************************)
+
+(* [fail] is a failure/exception handler *)
+let fail = fun _ -> assert false
+
+(* [get_element_by_id id] gets a DOM element by its id *)
+let get_element_by_id id =
+  Js.Opt.get (Html.document##getElementById (js id)) fail
+
+(* [append_text e s] appends string s to element e *)
+let append_text e s = Dom.appendChild e (document##createTextNode (js s))
+
+
+
+(* [main ()] is begins game execution by building the
+ * GUI and then calling the REPL function *)
+let main () =
+  let gui = get_element_by_id "gui" in

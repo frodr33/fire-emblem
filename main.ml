e075055a3620d81a7ebf04b3087fbc2b25d37f24
fsr32 <fsr32@cornell.edu>
2018-04-04 23:16:04 -0400

    Made changes to main and added png files for logo and background

--- main.ml
+++ main.ml
@@ -4,6 +4,7 @@ module Html = Dom_html
 let js = Js.string (* partial function, takes in string *)
 let document = Html.document
 
+(* NOTE: Change this section to make it less similar to Zoldas *)
 (************************ DOM HELPERS ************************)
 
 (* [fail] is a failure/exception handler *)
@@ -18,9 +19,45 @@ let append_text e s = Dom.appendChild e (document##createTextNode (js s))
 
 
 
-(* [main ()] is begins game execution by building the
- * GUI and then calling the REPL function *)
+(* [main ()] is begins game execution by first building and designing
+ * the html page and designing and subsequently calling the REPL to
+ * start execution using the game engine *)
 let main () =
   let gui = get_element_by_id "gui" in
-  let h1 = Html.createH1 document in
-  append_text h1 "Fire Emblem"
+  let body = get_element_by_id "body" in
+  let logo = Html.crateImg document in
+  let p1 = Html.createP document in
+  let p2 = Html.createP document in
+  let p3 = Html.createP document in
+  let audio = Html.createAudio document in
+  let canvas = Html.createCanvas document in
+  gui##style##textAlign <- js "center";
+  body##style##backgroundImage <-js "url('sprites/background.png')";
+  body##style##backgroundRepeat <- js "no-repeat";
+  logo##src <- js "sprites/Logo.png";
+  (* audio##src <- js "FireEmblem.mp3";
+  audio##play (); *)
+  gui##style##cssText <- js "font-size:16px";
+  gui##style##textAlign <- js "center";
+  canvas##width <- int_of_float Gui.canvas_width;
+  canvas##height <- int_of_float Gui.canvas_height;
+  append_text p "Welcome to Fire Emblem! Some stuff about the game ...";
+  append_text p2 "Developed by: Frank Rodriguez, Albert Tsao, Darren Tsai, and Ray Gu";
+  append_text p3 "for our 3110 final project. Thanks for playing!";
+  Dom.appendChild gui logo;
+  Dom.appendChild gui p;
+  Dom.appendChild gui canvas;
+  Dom.appendChild gui p2;
+  Dom.appendChild gui p3;
+
+  
+  (* let _ = Html.addEventListener
+      document Html.Event.keydown (Html.handler Game.keydown)
+      Js._true in
+  let _ = Html.addEventListener
+      document Html.Event.keydown (Html.handler Game.keyup)
+      Js._true in
+  Game.game_loop context false *)
+
+(* Begin the game *)
+let _ = main ()
